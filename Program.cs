3a92291f74c7f16458d3a7623b8a2e47fcb76050
Salvador <sarevalotamayo@student.rtc.edu>
2024-01-10 20:31:41 -0800

    Update Program File

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ namespace GA_WriteLineSalvador
     {
         static void Main(string[] args)
         {
-            Console.Write("Edsger W. Dijkstra");
-            Console.WriteLine("Computer science is no more about computers than astronomy is about telescopes.");
-            Console.Write("Alan Kay");
-            Console.WriteLine("The best way to predict the future is to invent it.");
-            Console.Write("Grace Hopper");
-            Console.WriteLine("The most damaging phrase in the language is, We’ve always done it this way.");
-            Console.Write("Terry Pratchett (Discworld");
-            Console.WriteLine("It's not worth doing something unless someone, somewhere, would much rather you weren't doing it.");
+            Console.Write("Edsger W. Dijkstra.");
+            Console.WriteLine(" Computer science is no more about computers than astronomy is about telescopes.");
+            Console.Write("Alan Kay.");
+            Console.WriteLine(" The best way to predict the future is to invent it.");
+            Console.Write("Grace Hopper.");
+            Console.WriteLine(" The most damaging phrase in the language is, We’ve always done it this way.");
+            Console.Write("Terry Pratchett.");
+            Console.WriteLine(" It's not worth doing something unless someone, somewhere, would much rather you weren't doing it.");
         }
     }
 }
